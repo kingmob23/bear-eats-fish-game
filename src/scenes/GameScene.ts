4d3bebd8abcc9619d936178cca42a95fdd94740d
yaroslav.safonov <safonov.ya@mxm.email>
2024-08-04 21:46:05 +0300

    fix images

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -13,10 +13,13 @@ class GameScene extends Phaser.Scene {
     }
 
     create() {
-        this.add.image(400, 300, 'background');
+        const background = this.add.sprite(400, 300, 'background');
+        background.setDisplaySize(800, 600);
+
         this.bear = this.add.sprite(400, 300, 'bear');
         console.log('Game scene created');
     }
+
 
     update() {
         this.bear.x += 1;
