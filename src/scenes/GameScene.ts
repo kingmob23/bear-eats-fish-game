2ce30116da3b4c9513a90b362d15fcf501947c7b
yaroslav.safonov <safonov.ya@mxm.email>
2024-08-05 11:39:17 +0300

    add splash and action button

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -2,11 +2,16 @@ import Phaser from 'phaser';
 
 class GameScene extends Phaser.Scene {
     private bear!: Phaser.GameObjects.Sprite;
+    private actionButton!: Phaser.GameObjects.Sprite;
+    private splash!: Phaser.GameObjects.Sprite;
     private backgroundMusic!: Phaser.Sound.BaseSound;
     private moveSound!: Phaser.Sound.BaseSound;
+    private splashSound!: Phaser.Sound.BaseSound;
+    private buttonActive: boolean;
 
     constructor() {
         super({ key: 'GameScene' });
+        this.buttonActive = false;
     }
 
     preload() {
@@ -14,37 +19,87 @@ class GameScene extends Phaser.Scene {
         this.load.image('background', 'assets/images/background.webp');
         this.load.audio('backgroundMusic', 'assets/audio/background.mp3');
         this.load.audio('moveSound', 'assets/audio/move.mp3');
+        this.load.image('actionButton', 'assets/interface/action-button.svg');
+        this.load.image('splash', 'assets/images/splash.webp');
+        this.load.audio('splashSound', 'assets/audio/splash-sound.mp3');
     }
 
     create() {
-        const background = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'background');
-        background.setDisplaySize(this.cameras.main.width, this.cameras.main.height);
+        const { width, height } = this.cameras.main;
 
-        this.bear = this.add.sprite(this.cameras.main.width / 2, this.cameras.main.height / 2, 'bear');
+        const background = this.add.image(width / 2, height / 2, 'background') as Phaser.GameObjects.Image;
+        background.setDisplaySize(width, height);
+
+        this.bear = this.add.sprite(width / 2, height / 2, 'bear');
+
+        const buttonScale = 1.7;
+        const actionButtonPaddingX = width * 0.05;
+        const actionButtonPaddingY = height * 0.05;
+        const finalX = width - actionButtonPaddingX - ((this.textures.get('actionButton').getSourceImage().width * buttonScale) / 2);
+        const finalY = height - actionButtonPaddingY - ((this.textures.get('actionButton').getSourceImage().height * buttonScale) / 2);
+        this.actionButton = this.add.sprite(finalX, finalY, 'actionButton').setScale(buttonScale);
+        this.actionButton.setAlpha(0.9);
+        this.actionButton.setInteractive();
+        this.actionButton.on('pointerdown', this.onActionButtonClick, this);
+
+        const splashVerticalOffset = height * 0.25;
+        this.splash = this.add.sprite(width / 2, splashVerticalOffset, 'splash');
+        this.splash.setScale(0.4);
+        this.splash.setVisible(false);
 
         this.backgroundMusic = this.sound.add('backgroundMusic', {
             loop: true,
             volume: 0.2
         });
-
         this.backgroundMusic.play();
 
-        this.moveSound = this.sound.add('moveSound', {
-            volume: 1.0
-        });
+        this.moveSound = this.sound.add('moveSound', { volume: 1.0 });
+        this.splashSound = this.sound.add('splashSound', { volume: 0.1 });
 
         this.input.on('pointerdown', this.moveBear, this);
+
+        const randomTime = Phaser.Math.Between(3000, 10000);
+        this.time.delayedCall(randomTime, this.showSplash, [], this);
+
+        this.setButtonActive(false);
+    }
+
+    onActionButtonClick(pointer: Phaser.Input.Pointer, localX: number, localY: number, event: Phaser.Types.Input.EventData) {
+        if (this.buttonActive) {
+            console.log('Button clicked!');
+            event.stopPropagation();
+
+            this.splash.setVisible(false);
+            if (this.splashSound.isPlaying) {
+                this.splashSound.stop();
+            }
+            this.backgroundMusic.resume();
+            this.setButtonActive(false);
+        }
+    }
+
+    setButtonActive(active: boolean) {
+        this.buttonActive = active;
+        if (active) {
+            this.actionButton.setAlpha(1);
+        } else {
+            this.actionButton.setAlpha(0.9);
+            this.tweens.killTweensOf(this.actionButton);
+        }
+    }
+
+    showSplash() {
+        const { width, height } = this.cameras.main;
+        this.splash.setPosition(width / 4, height - height / 4);
+        this.splash.setVisible(true);
+        this.backgroundMusic.pause();
+        this.splashSound.play();
+
+        this.setButtonActive(true);
     }
 
     moveBear(pointer: Phaser.Input.Pointer) {
-        // if (this.moveSound.isPlaying) {
-        //     this.moveSound.stop();
-        // }
-        // The moveBear method stops the sound if it is already playing before starting it again. This ensures that the sound effect starts from the beginning each time the bear moves.
-        // But it actually acts the same without it.
-        this.moveSound.play({
-            volume: 3.0
-        });
+        this.moveSound.play({ volume: 3.0 });
         this.tweens.add({
             targets: this.bear,
             x: pointer.x,
@@ -60,7 +115,17 @@ class GameScene extends Phaser.Scene {
     }
 
     update() {
-        // No need for continuous update logic here
+        if (this.splash.visible) {
+            const distance = Phaser.Math.Distance.Between(
+                this.bear.x, this.bear.y,
+                this.splash.x, this.splash.y
+            );
+            const activationDistance = this.splash.displayHeight;
+            this.setButtonActive(distance <= activationDistance);
+
+            this.bear.setDepth(this.bear.y > this.splash.y ? 1 : 0);
+            this.splash.setDepth(this.bear.y > this.splash.y ? 0 : 1);
+        }
     }
 }
 
--- src/scenes/StartScene.ts
+++ src/scenes/StartScene.ts
@@ -6,14 +6,12 @@ class StartScene extends Phaser.Scene {
     }
 
     create() {
-        console.log('StartScene create');
         const startText = this.add.text(this.cameras.main.width / 2, this.cameras.main.height / 2, 'Tap to Start', {
             fontSize: '32px',
             color: '#fff'
         }).setOrigin(0.5).setInteractive();
 
         startText.on('pointerdown', () => {
-            console.log('Start screen tapped');
             this.scene.start('GameScene');
         });
     }
